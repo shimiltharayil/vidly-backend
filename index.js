1dff77e1e17a5d4626f11dac7fe0d1bbdc3035fb
Shimil <shimiltharayil007@gmail.com>
2023-01-26 11:51:29 +0530

    vidly initial commit

--- index.js
+++ index.js
@@ -1,13 +1,67 @@
-const express = require('express');
+const Joi = require("joi");
+const express = require("express");
 const app = express();
 
+app.use(express.json());
 
-app.listen(3000, ()=>{
-    console.log("listening on port 3000...");
-})
-app.get("/", (req,res)=>{
-    res.send("Hi There!");
-})
-app.get("/api",(req,res)=>{
-    res.send([1,2,3,4,5,6])
+const courses = [
+  { id: 1, name: "Course1" },
+  { id: 2, name: "Course2" },
+  { id: 3, name: "Course3" },
+];
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`listening on port ${port}...`);
+});
+app.get("/", (req, res) => {
+  res.send("Hi There!");
+});
+app.get("/api/courses", (req, res) => {
+  res.send(courses);
+});
+app.get("/api/courses/:id", (req, res) => {
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course) return res.status(404).send("Wrong course");
+  res.send(course);
+});
+
+app.post("/api/courses", (req, res) => {
+  const { error } = validateCourse(req.body);
+  if (error) return;
+  res.status(404).send(error.details[0].message);
+
+  const course = {
+    id: courses.length + 1,
+    name: req.body.name,
+  };
+  courses.push(course);
+  res.send(course);
+});
+app.put("/api/courses/:id", (req, res) => {
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course) return res.status(404).send("Wrong course");
+
+  const { error } = validateCourse(req.body);
+  if (error) {
+    res.status(404).send(error.details[0].message);
+    return;
+  }
+  course.name = req.body.name;
+  res.send(course);
+  // .put
+});
+
+const validateCourse = (course) => {
+  const schema = {
+    name: Joi.string().min(3).required(),
+  };
+  return Joi.validate(course, schema);
+};
+app.delete("/api/courses/:id", (req, res) => {
+  const course = courses.find((c) => c.id === req.params.id);
+  if (!course) return res.status(404).send("Invalid course id");
+  const index = courses.indexOf(course);
+  courses.splice(index, 1);
+  res.send(course);
 });
