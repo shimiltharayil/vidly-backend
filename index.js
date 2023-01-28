14466ca15f86dc13b4bb48209261f342541133af
Shimil <shimiltharayil007@gmail.com>
2023-01-28 15:34:24 +0530

    models added

--- index.js
+++ index.js
@@ -37,3 +37,4 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`listening on port ${port}...`);
 });
+
--- routes/genres.js
+++ routes/genres.js
@@ -1,48 +1,47 @@
 const express = require("express");
 const router = express.Router();
 const Joi = require("joi");
+const { Genre } = require("../models/genre");
+const { Movie } = require("../models/movie");
 
-const genres = [
-  { id: 1, name: "Action" },
-  { id: 2, name: "Horror" },
-  { id: 3, name: "Romance" },
-];
-router.get("/", (req, res) => {
-  res.send(genres);
-});
-
-router.post("/", (req, res) => {
-  const { error } = validateGenres(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
-
-  const genre = {
-    id: genres.length + 1,
-    name: req.body.name,
-  };
-  genres.push(genre);
-  res.send(genres);
-});
-
-router.put("/:id", (req, res) => {
-  const genre = genres.find((g) => g.id === req.params.id);
-  if (!genre) return res.status(404).send("Wrong Genre");
-  genre.name = req.body.name;
+router.get("/", async (req, res) => {
+  const genre = await Genre.find().sort("name");
   res.send(genre);
 });
 
-const validateGenres = (genre) => {
-  const schema = {
-    name: Joi.string().min().required(),
-  };
-  return Joi.validate(genre, schema);
-};
+router.post("/", async (req, res) => {
+  const { error } = validateGenres(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
 
-router.get("/:id", (req, res) => {
-   const genre = genres.find(g => g.id === req.params.id)
-  if (!genre) return res.status(404).send("Wrong Genre");
-   const index = genres.indexOf(genre)
-   genres.splice(index, 1)
-   res.send(genre)
+  const genre = await Genre.findById(req.body.genreId);
+  if (!genre) return res.status(404).send(error.details[0].message);
+  let movie = new Movie({
+    title: req.body.title,
+    genre: {
+      _id: genre._id,
+      name: genre.name,
+    },
+    numberInStock: req.body.numberInStock,
+    dailyRentalRate: req.body.dailyRentalRate,
+  });
+  movie = movie.save();
 });
 
+router.put("/:id", async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+  const genre = await Genre.findByIdAndUpdate(
+    req.params.genreId,
+    { name: req.body.name },
+    { new: true }
+  );
+  if (!genre)
+    return res.status(404).send("The  genre with the given ID was not found");
+  res.send(genre);
+});
+router.delete("/:id", async (req, res) => {
+  const genre = Genre.findByIdAndRemove(req.params.id);
+  if (!genre)
+    return res.status(404).send("The  genre with the given ID was not found");
+});
 module.exports = router
